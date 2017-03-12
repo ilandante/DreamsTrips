e4efb8e8989383b7763e3a0e872b7e5fd82f007a
ilandante <rafasylver@hotmail.com>
2017-03-12 15:54:06 -0600

    Changed db user in settings

--- DreamsTrips/settings.py
+++ DreamsTrips/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'dreams_db',
         'USER': 'postgres',
-        'PASSWORD': 'fadetoblack13',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '',
     }
