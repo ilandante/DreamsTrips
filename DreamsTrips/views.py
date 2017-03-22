74f0680402815bdd2229c9cb73e92b035d844e23
ilandante <rafasylver@hotmail.com>
2017-03-21 22:36:15 -0600

    Set yoviajocondreamstrips mail

--- DreamsTrips/views.py
+++ DreamsTrips/views.py
@@ -70,7 +70,7 @@ def send_contact_message(request):
             'Web Page Comment',
             message,
             'yoviajocondreamstrips@gmail.com',
-            ['rmoreno.ter@gmail.com'],
+            ['yoviajocondreamstrips@gmail.com'],
             fail_silently=False,
         )
 
@@ -94,7 +94,7 @@ def send_quote_request(request):
             'Cotization Request',
             message,
             'yoviajocondreamstrips@gmail.com',
-            ['rmoreno.ter@gmail.com'],
+            ['yoviajocondreamstrips@gmail.com'],
             fail_silently=False,
         )
 
